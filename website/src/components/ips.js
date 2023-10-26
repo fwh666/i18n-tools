f06222a331b2778302e9cde63729adbd5aac83a4
ext.fuwenhao3 <ext.fuwenhao3@jd.com>
2023-10-26 13:11:22 +0800

    fix: title style text-center

--- website/src/components/ips.js
+++ website/src/components/ips.js
@@ -57,8 +57,8 @@ const IpAddress = () => {
     return (
         <div className="container mx-auto">
             {/*<h1 className="text-3xl font-bold text-center mt-8 mb-4">IP Address Lookup</h1>*/}
-            <h1 className="text-5xl font-bold tracking-tight mb-4">IP Address Lookup</h1>
-            <p className="text-base font-normal">"Whoer IP" is an online service by whoer.net, enabling users to easily check their current IP address. It provides detailed IP information like the location, country, and Internet Service Provider. This tool serves a key role in improving users' understanding of their own online presence and maintaining internet privacy and security.</p>
+            <h1 className="text-5xl font-bold text-center tracking-tight mb-4">IP Address Lookup</h1>
+            <p className="text-base text-center font-normal">"Whoer IP" is an online service by whoer.net, enabling users to easily check their current IP address. It provides detailed IP information like the location, country, and Internet Service Provider. This tool serves a key role in improving users' understanding of their own online presence and maintaining internet privacy and security.</p>
             <form onSubmit={handleSubmit} className="max-w-md mx-auto">
                 <div className="flex items-center border-b border-b-2 border-blue-500 py-2">
                     <input
